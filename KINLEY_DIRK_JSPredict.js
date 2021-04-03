482ec6d49d7f434fa862584cf57ca3f87363a59e
Dirk Kinley <42229326+dkinley@users.noreply.github.com>
2021-04-02 21:54:45 -0700

    Add files via upload

---
+++ KINLEY_DIRK_JSPredict.js
@@ -0,0 +1,26 @@
+function myBirthYearFunc(){
+    console.log("I was born in " + 1980);
+}
+// Predict 1
+// I was born in 1980
+
+
+function myBirthYearFunc(birthYearInput){
+    console.log("I was born in " + birthYearInput);
+}
+// Predict 2
+// I was born in 1980
+
+
+function add(num1, num2){
+    console.log("Summing Numbers!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+// Predict 3
+// Summing Numbers!
+// num1 is: 10
+// num2 is: 20
+// 30
