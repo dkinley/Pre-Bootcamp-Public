7261c3559b2681adc978a5e743e47ed759d5ee35
Dirk Kinley <42229326+dkinley@users.noreply.github.com>
2021-04-04 08:52:48 -0700

    Add files via upload

---
+++ KINLEY_DIRK_JS_TINKERING.js
@@ -0,0 +1,11 @@
+function displayIfChildIsAbleToRideTheRollerCoaster(childHeight) {
+
+    if (childHeight <= 52) {
+        console.log("Sorry kiddo. Maybe next year.");     
+    } else {
+        console.log("Get on that ride, kiddo!");
+    }   
+    }
+
+displayIfChildIsAbleToRideTheRollerCoaster(30); // run the function
+displayIfChildIsAbleToRideTheRollerCoaster(53); // run the function again
